469c323487839709816f0334f61822939ffa57d9
amanas96 <amanghatampur@gmail.com>
2025-11-03 09:48:48 +0530

    TutorArch

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -6,10 +6,13 @@ function HomePage() {
 
   const handleStartSession = async () => {
     try {
-      const response = await fetch("https://tutorarch.onrender.com/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      });
+      const response = await fetch(
+        "https://tutorarch.onrender.com/api/sessions/create",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       if (!response.ok) throw new Error("Network response was not ok");
       const data = await response.json();
 
